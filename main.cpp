418731f76ccf4dfecd9d48ad968684a462aa7fe5
lthornton1 <lthornton1@mail.csuchico.edu>
2018-02-13 20:58:25 -0800

    test1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Logen Thornton
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,16 @@ using namespace std;
 int main()
 {
   //declare variables
+string first = "";
+string last = "";
 
   //get user input
+cout << "Please enter your first name." << endl;
+cin >> first;
+cout << "Please enter your last name." << endl;
+cin >> last;
 
+cout << first << " " << last << endl;
   //tell fortune
 
   return 0;
